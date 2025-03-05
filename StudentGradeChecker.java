60ee271d485d4242f6b6c214af82b6f28c573af4
EmyuJay <mjjnrr18@gmail.com>
2025-03-05 21:26:41 +0800

    Add files via upload

---
+++ StudentGradeChecker.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class StudentGradeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your grade (A-F): ");
+        char grade = Character.toUpperCase(scanner.next().charAt(0)); // Ensures case insensitivity
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'B':
+                System.out.println("Good job!");
+                break;
+            case 'C':
+                System.out.println("You passed.");
+                break;
+            case 'D':
+                System.out.println("Needs improvement.");
+                break;
+            case 'F':
+                System.out.println("Failed.");
+                break;
+            default:
+                System.out.println("Invalid grade. Please enter a valid grade (A-F).");
+        }
+
+        scanner.close();
+    }
+}
