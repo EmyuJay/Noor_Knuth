925f277ed23232b2d654ee2fa238093b81327552
EmyuJay <mjjnrr18@gmail.com>
2025-04-11 20:50:12 +0800

    Add files via upload

---
+++ PersonDemo.java
@@ -0,0 +1,33 @@
+class Person {
+    // Attributes
+    String name;
+    int age;
+    String city;
+
+    // Constructor
+    Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    // Method to display details
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+        System.out.println();
+    }
+}
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        // Creating two Person objects
+        Person person1 = new Person("Lorenzo", 17, "Davao");
+        Person person2 = new Person("Vincentino", 18, "Davao");
+
+        // Displaying details of each person
+        person1.displayDetails();
+        person2.displayDetails();
+    }
+}
