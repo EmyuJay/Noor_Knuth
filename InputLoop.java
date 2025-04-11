7bba39eec5a95ea8147ab79ddbac0b6e8639a8b7
EmyuJay <mjjnrr18@gmail.com>
2025-04-11 20:45:55 +0800

    Add files via upload

---
+++ InputLoop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InputLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter a word (type 'exit' to stop): ");
+            String input = scanner.next();
+
+            if (input.equals("exit")) {
+                System.out.println("Program stopped.");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
